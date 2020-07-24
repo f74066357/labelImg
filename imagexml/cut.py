10064c4e1b0c7d8f22a8f47e29f1c40598461204
f74066357 <f74066357@gs.ncku.edu.tw>
2020-07-24 21:32:02 +0800

    xml to text

---
+++ imagexml/cut.py
@@ -0,0 +1,49 @@
+# coding=Big5
+n=input('num¡G')
+total=input('total¡G')
+k=1
+while(k<=int(total)):
+  if (k<10):
+    t='0'+str(k)
+  else:
+    t=str(k)
+    
+  filename='fb'+n+'_'+t+'.xml'
+  print(filename)
+  f = open(filename, 'r')
+  fb = open("fb.txt", 'a')
+  lines_n = [lines.rstrip('\r\n') for lines in f.readlines() if lines.strip()]
+  for i, line in enumerate(lines_n):
+    if(len(line)>10):
+      test=line[4]+line[5]+line[6]+line[7]
+      if(test=='lena'):
+        #print("ok")
+        front=line.split('>')[1]
+        back=front.split('<')[0]
+        #print(back)
+        fb.write(back+" ")
+      if(test=='xmin'):
+        front=line.split('>')[1]
+        back=front.split('<')[0]
+        #print(back)
+        fb.write(back+" ")
+      if(test=='ymin'):
+        front=line.split('>')[1]
+        back=front.split('<')[0]
+        #print(back)
+        fb.write(back+" ")
+      if(test=='xmax'):
+        front=line.split('>')[1]
+        back=front.split('<')[0]
+        #print(back)
+        fb.write(back+" ")
+      if(test=='ymax'):
+        front=line.split('>')[1]
+        back=front.split('<')[0]
+        #print(back)
+        fb.write(back)
+        fb.write('\n')
+  k=k+1
+  #f.close()
+#fb.close()
+  
